0f86422c49934618922aa1a2726c102ada201146
wangzk <wangzk@suooter.com>
2019-12-19 10:58:05 +0800

    修改sln位置，添加npoi使用demo的一个方法

--- Tools/NpoiHelperDemo.cs
+++ Tools/NpoiHelperDemo.cs
@@ -50,5 +50,50 @@ namespace Tools
             //一定要有这一行
             myDataset.Tables.Remove(myDataTable);
         }
+		
+        /*导入excel
+		private void ImportBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            string ExcleFilePath = string.Empty;
+            if (ofd.ShowDialog() == true)
+            {
+                ExcleFilePath = ofd.FileName;
+            }
+            else
+            {
+                return;
+            }
+
+            try
+            {
+                NpoiHelper npoiHelper = new NpoiHelper();
+                var excelTable = npoiHelper.GetSheetTable(ExcleFilePath,0,true);
+
+                List<AttendancePerson> People = new List<AttendancePerson>();
+                for (int i = 0; i < excelTable.Rows.Count; i++)
+                {
+                    if (i == 0)
+                    {
+                        continue;//跳过首行标题
+                    }
+                    AttendancePerson p = new AttendancePerson()
+                    {
+                        Name = excelTable.Rows[i][1] == null ? "" : excelTable.Rows[i][1].ToString(),
+                        IDNum = excelTable.Rows[i][2] == null ? "" : excelTable.Rows[i][2].ToString(),
+                        PhoneNum = excelTable.Rows[i][4] == null ? "" : excelTable.Rows[i][4].ToString(),
+                        Org = excelTable.Rows[i][3] == null ? "" : excelTable.Rows[i][3].ToString(),
+                    };
+                    People.Add(p);
+                }
+                CustomerConferenceDB.GetInstance().AddPerson(People);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, ex.TargetSite.ToString());
+            }
+
+        }
+        */
     }
 }
